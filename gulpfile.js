3696d72452f571c2b9e911fa4b3679d7ce3035c8
unknown <>
2017-08-12 10:49:45 +0800

    改进生成ES5,压缩ES5

--- gulpfile.js
+++ gulpfile.js
@@ -4,23 +4,43 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     pump = require('pump');
 
-gulp.task('default', function () {
-    gulp.src('src/FileSystem.js')
+gulp.task('es5', function () {
+    gulp.src('src/FileSystem.js') 
         .pipe(gulp.dest('dist'))
-        .pipe(gulp.dest('docs/demo'))
-        .pipe(babel({
+        .pipe(babel({              
             presets: ['es2015']
         }))
         .pipe(rename('FileSystem.ES5.js'))
         .pipe(gulp.dest('dist'))
-        .pipe(gulp.dest('docs/demo'))
-        .pipe(rename('FileSystem.ES5.min.js'))
+})
+
+gulp.task('min', function () {   
+    gulp.src('dist/FileSystem.js')
+        .pipe(rename('FileSystem.min.js'))
+        //.pipe(uglify())
         .pipe(gulp.dest('dist'))
+})
+
+gulp.task('min-es5', function () {
+    // 压缩ES5版本
+    gulp.src('dist/FileSystem.ES5.js')
+        .pipe(rename('FileSystem.ES5.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'))
+
+})
+
+gulp.task('copy', function () {
+    gulp.src('dist/**')
         .pipe(gulp.dest('docs/demo'))
-});
+})
+
+
 gulp.task('watch', function () {
     gulp.watch('src/FileSystem.js', ['ES6ToES5'])
 });
+
+/*
 gulp.task('compress', function (cb) {
     pump([
         gulp.src('dist/FileSystem.ES5.min.js'),
@@ -29,4 +49,7 @@ gulp.task('compress', function (cb) {
     ],
         cb
     );
-});
+}); */
+
+
+gulp.task('default',['es5','min','min-es5','copy'])
